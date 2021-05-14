e397d31261a6db0843acc62e819ada37766c8c55
Dhruv Tyagi <dhruvtyagi27071996@gmail.com>
2021-05-14 11:27:37 +0530

    new file:   .gitignore 	modified:   README.md 	new file:   ormconfig.json 	new file:   package-lock.json 	new file:   package.json 	new file:   src/entity/User.ts 	new file:   src/index.ts 	new file:   tsconfig.json

---
+++ src/entity/User.ts
@@ -0,0 +1,34 @@
+import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, CreateDateColumn, UpdateDateColumn, BeforeInsert} from "typeorm";
+
+import {v4 as uuid} from 'uuid'
+
+@Entity("users")
+export class User extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    role: string;
+
+    @Column()
+    email: string;
+
+    @Column({type: 'uuid'})
+    uuid: string
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @BeforeInsert()
+    createUuid(){
+        this.uuid=uuid()
+    }
+
+}
---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
+
+import express, { Request, Response } from 'express'
+
+
+const app=express()
+app.use(express.json())
+
+
+//CREATE
+app.post('/users',async(req:Request,res: Response)=>{
+    const {name, role, email}=req.body
+    try{
+        const user=User.create({name,role,email})
+        await user.save()
+        return res.status(201).json(user)
+    }catch(err){
+        console.log(err)
+        return res.status(500).json(err)
+    }
+})
+
+
+//READ
+
+app.get('/users',async(_:Request,res: Response)=>{
+   
+    try{
+        const users=await User.find()
+
+        return res.json(users)
+    }catch(err){
+        console.log(err)
+        return res.status(500).json(err)
+    }
+})
+
+//UPDATE
+//DELETE
+//FIND
+
+createConnection().then(async() => {
+
+   app.listen(5000,()=>console.log('Server up at http://localhost:5000'))
+    
+}).catch(error => console.log(error));
