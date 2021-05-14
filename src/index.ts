a935f2604fc60c2daadc28eaba5e645579b301d9
Dhruv Tyagi <dhruvtyagi27071996@gmail.com>
2021-05-14 15:23:08 +0530

    cccc

--- src/index.ts
+++ src/index.ts
@@ -90,6 +90,7 @@ app.delete('/users/:uuid',async (req: Request,res: Response)=>{
     }
 })
 
+//https://stackoverflow.com/questions/50093144/mysql-8-0-client-does-not-support-authentication-protocol-requested-by-server
 
 //FIND USERS BY ID
 
